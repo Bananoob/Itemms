2e1987c70eb31f22c0e09d2e518d62efefbc28b7
Bananoob <mathis.jardon@gmail.com>
2023-07-19 11:33:27 +0200

    Added filter section

--- main.js
+++ main.js
@@ -6,6 +6,18 @@ let pickaxes = [
     
 ]
 
-let weapons = [
+class Weapons {
+    constructor(config) {
+        this.config = config;
+        this.name = config.name;
+        this.id = config.id;
+        this.img = config.img;
+    }
+}
 
-]
+let add = document.querySelector('.add')
+let noweaponsfound = document.querySelector('.no-weapons-found')
+let wrapperbutton = document.querySelector('.wrapper-button')
+
+wrapperbutton.style.display = "none"
+noweaponsfound.style.display = "none"
